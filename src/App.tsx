85338c54eddc69a16716253494b8bc26aee7a0e7
Chech Alvarez <chech.alvarez@gmail.com>
2025-01-21 12:31:44 -0600

    feat: home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router'
+
 import GradientDiv from './components/GradientDiv'
 import MainContainer from './components/MainContainer'
+import Home from './components/Home'
 
 function App() {
   return (
-    <GradientDiv className={'bg-hazy-sky-900 w-screen max-w-full min-h-dvh'}>
-      <MainContainer />
-    </GradientDiv>
+    <BrowserRouter>
+      <GradientDiv className={'bg-hazy-sky-900 w-screen max-w-full min-h-dvh'}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/my-portfolio" element={<MainContainer />} />
+        </Routes>
+      </GradientDiv>
+    </BrowserRouter>
   )
 }
 
---
+++ src/components/Home/index.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="text-slate-100 text-6xl min-h-dvh max-w-screen-xl m-auto flex flex-1 flex-col justify-center items-center gap-4 py-6 p-5 sm:px-12 lg:flex-col lg:px-8 lg:p-0">
+      <div>Flychech</div>
+      <div className="text-2xl">Under construction...</div>
+    </div>
+  )
+}
